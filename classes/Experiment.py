72e1b436c30b11fdeff39de9cb7b5fd22413663e
thereisnospoon <segal.guy@gmail.com>
2019-09-20 15:29:25 +0300

    finished basic run_experiment

--- classes/Experiment.py
+++ classes/Experiment.py
@@ -3,8 +3,10 @@ import os
 
 
 class Experiment:
-    def __init__(self, name):
+    def __init__(self, name, full_path, model, dataset_path, data_iterator):
         self.name = name
+        # Todo: assert name of experiment is unique
+        self.full_path = full_path
         pass
 
     def run_train(self):
--- classes/ExperimentManager.py
+++ classes/ExperimentManager.py
@@ -1,10 +1,33 @@
 import sys
+import json
+import os
+from Experiment import Experiment
 
 
 class ExperimentManager:
-    def __init__(self, name):
+    def __init__(self, name, full_path):
+        """
+        :param name: Name of experiment.
+        :param full_path: full path to experiment.
+        """
         self.name = name
-        pass
+        self.full_path = full_path
+        self.experiments = []
 
-    def run_experiment(self):
-        pass
+    def run_experiment(self, name, model, data_path, data_iterator):
+        """
+        :param name: Name of run.
+        :param model: Python function that is the model (keras/tensorflow)
+        :param data_path: Path to dataset
+        :param data_iterator: Iterator object that will yield data for training or testing accordingly
+        :return:
+        """
+        experiment = Experiment(name, os.path.join(self.full_path, name), model, data_path, data_iterator)
+        self.experiments.append(experiment.full_path)
+
+
+if __name__ == '__main__':
+   e = ExperimentManager('temp', '~/Projects/coding_project/ExperimentManager/')
+   e.run_experiment('tepmp', min, '/x/x/x', object)
+   x = json.dumps(e.__dict__)
+   print(x)
